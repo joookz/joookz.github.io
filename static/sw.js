189e9b20ba4ac87c309d78082c1b829cdb8a0e55
lockness-Ko <videolk9373@gmail.com>
2021-09-17 10:50:35 +1000

    lol?

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('install', function (event) {
-
+    console.error("AAAAAAAAAAAAAAA");
 });
 
 self.addEventListener('fetch', function (event) {
+    console.error("AAAAAAAAAAAAAAA");
+    const url = new URL(event.request.url)
     event.respondWith(
         new Response(
             "Hello World!", {
