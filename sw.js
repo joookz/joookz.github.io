a57132bccd2ac274282bd0be533050251077c2e5
lockness-Ko <videolk9373@gmail.com>
2021-09-17 11:25:11 +1000

    fixed the service worker again!

--- sw.js
+++ sw.js
@@ -5,11 +5,17 @@ self.addEventListener('install', function (event) {
 self.addEventListener('fetch', function (event) {
     const url = new URL(event.request.url);
     console.log(url, location)
-    event.respondWith(
-        new Response(fetch("https://jokzbackend.locknessko.repl.co/api/v1/get?id=" + url.href.split(url.hostname)[1].split("#/")[1]).then(x=>{return x.text()}).then(y=>{return y}), {
-            headers: {
-                'Content-Type': 'text/html'
-            }
-        })
-    )
+    if (location.href.split(location.hostname)[1].includes("#") && location.href.split(location.hostname)[1].split("#/")[1] != undefined) {
+        event.respondWith(
+            new Response(fetch("https://jokzbackend.locknessko.repl.co/api/v1/get?id=" + url.href.split(url.hostname)[1].split("#/")[1]).then(x => {
+                return x.text()
+            }).then(y => {
+                return y
+            }), {
+                headers: {
+                    'Content-Type': 'text/html'
+                }
+            })
+        )
+    }
 });
