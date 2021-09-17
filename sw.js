f55906d1526c2963d6458faf18dccacdf4fe164e
lockness-Ko <videolk9373@gmail.com>
2021-09-17 11:30:17 +1000

    lol rmeoveing logging

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
     const url = new URL(event.request.url);
-    console.log(url, location)
     if (location.href.split(location.hostname)[1].includes("#") && location.href.split(location.hostname)[1].split("#/")[1] != undefined) {
         event.respondWith(
             fetch("https://jokzbackend.locknessko.repl.co/api/v1/get?id=" + url.href.split(url.hostname)[1].split("#/")[1]).then(x => {
