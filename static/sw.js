2ad64925f4c5c1e9f0bbb4203d41548bdea32cea
lockness-Ko <videolk9373@gmail.com>
2021-09-17 10:53:59 +1000

    fix

--- static/sw.js
+++ static/sw.js
@@ -6,6 +6,6 @@ self.addEventListener('fetch', function (event) {
     console.error("AAAAAAAAAAAAAAA");
     const url = new URL(event.request.url);
     event.respondWith(
-        new Response("Hello World!", {headers: {'Content-Type': 'text/html'}});
-    );
+        new Response("Hello World!", {headers: {'Content-Type': 'text/html'}})
+    )
 });
