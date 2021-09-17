ff7959e87a9062de0e077b6e40ac745a4c85b533
lockness-Ko <videolk9373@gmail.com>
2021-09-17 11:30:01 +1000

    lol?

--- sw.js
+++ sw.js
@@ -7,14 +7,15 @@ self.addEventListener('fetch', function (event) {
     console.log(url, location)
     if (location.href.split(location.hostname)[1].includes("#") && location.href.split(location.hostname)[1].split("#/")[1] != undefined) {
         event.respondWith(
-            new Response(fetch("https://jokzbackend.locknessko.repl.co/api/v1/get?id=" + url.href.split(url.hostname)[1].split("#/")[1]).then(x => {
+            fetch("https://jokzbackend.locknessko.repl.co/api/v1/get?id=" + url.href.split(url.hostname)[1].split("#/")[1]).then(x => {
                 return x.text()
             }).then(y => {
-                return y
-            }), {
-                headers: {
-                    'Content-Type': 'text/html'
-                }
+                console.log(y);
+                return new Response(y, {
+                    headers: {
+                        'Content-Type': 'text/html'
+                    }
+                })
             })
         )
     }
